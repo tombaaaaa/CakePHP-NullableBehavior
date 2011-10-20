002a6ab339d0b2288e3a08f005c61b5255cfe32d
Tom Brown <tombaaaaa@hotmail.com>
2011-10-20 08:23:51 +0100

    Updated README file

--- NullableBehavior.php
+++ NullableBehavior.php
@@ -2,7 +2,7 @@
 
 class NullableBehavior extends ModelBehavior {
 	/**
-	 * function beforeSave
+	 * public function beforeSave
 	 * 
 	 * Looks for nullable fields in the schema and replaces empty string values for those fields
 	 * with NULL values. This is helpful as hell when foreign key values are nullable lest you
@@ -11,7 +11,7 @@ class NullableBehavior extends ModelBehavior {
 	 * @param		model		The model object to be saved.
 	 * @return	void
 	 */
-	function beforeSave ($model) {
+	public function beforeSave ($model) {
 		$schema = $model->schema();
 
 		foreach ($schema as $field => $metadata) {
